fb85883d2ae7e82abe6c1ee772f573306831be38
Jack Meyer <jackcmeyer@gmail.com>
2019-07-22 00:02:10 -0500

    Iniital Commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  parser:  '@typescript-eslint/parser', 
+  extends:  [
+    'airbnb-typescript/base',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
